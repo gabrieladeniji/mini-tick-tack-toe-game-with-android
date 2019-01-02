5f156250fb9037bd4ad9c6930903da1c6d87c9f1
Gabriel <gabrieladeniji26@gmail.com>
2019-01-02 12:34:28 +0100

    Added a new TextView won message

--- app/src/main/java/com/tech104/isreal/connect3/MainActivity.java
+++ app/src/main/java/com/tech104/isreal/connect3/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Winning position
     int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+
+    // Won message display
+    TextView wonMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +51,13 @@ public class MainActivity extends AppCompatActivity {
                     gameStates[winningPosition[1]] == gameStates[winningPosition[2]] &&
                     gameStates[winningPosition[0]] != 0) {
                 String playerName = (gameStates[winningPosition[0]] == 1 ? "Player Yellow" : "Player Red");
-                Toast.makeText(this, playerName + " Won the game!", Toast.LENGTH_SHORT).show();
+                wonMessageTextView = (TextView) findViewById(R.id.wonMessageTextView);
+                String message = playerName + " Won the game";
+                wonMessageTextView.setText(message);
+                wonMessageTextView.setVisibility(View.VISIBLE);
+                wonMessageTextView.setScaleX(0.1f);
+                wonMessageTextView.setScaleY(0.1f);
+                wonMessageTextView.animate().scaleX(1).scaleY(1).setDuration(2000);
                 this.disableAllCounter();
                 Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
                 playAgainButton.setVisibility(View.VISIBLE);
@@ -87,5 +97,8 @@ public class MainActivity extends AppCompatActivity {
             counter.setEnabled(true);
             counter.setImageResource(0);
         }
+        // Reset the won Message
+        wonMessageTextView.setVisibility(View.GONE);
+        wonMessageTextView.setText("");
     }
 }
